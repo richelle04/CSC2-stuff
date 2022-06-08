e7a00c6e39fee7deec8ab3b863970deb91d2a768
richelle <st21374@ormiston.school.nz>
2022-06-09 11:29:00 +1200

    Started coding

--- Assessment.py
+++ Assessment.py
@@ -1,1 +1,15 @@
-print("www.google.com")
+#This program is for Julie to put the details from party hire store
+
+# import tkinter so I can make a GUI
+from tkinter import *
+
+root = Tk()
+root.title('Welcome To Party Hire Store!')
+
+def quit():
+    main_window.destroy()
+
+# print details of all the camps 
+def print_camp_details():
+    
+
